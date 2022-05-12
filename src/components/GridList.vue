5be9a2ce0777fbf1a1f4de1c1bdae87c83110ca3
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-05-12 17:47:20 +0200

    wip in crash

--- src/components/GridList.vue
+++ src/components/GridList.vue
@@ -2,37 +2,37 @@
   <section>
     <div class="container d-flex flex-wrap">
     <h2>{{title}}</h2>
-      <div class="card row row-cols-5 debug">
-        <div v-for="(item) in items" :key="item.id" class="">
-          <div>
+      <div class="card-group debug">
+        <div v-for="(item) in items" :key="item.id" class="card">
             <div class="debug"><img :src="'https://image.tmdb.org/t/p/w342/'+ item.poster_path" :alt="item.title" class="card-img-top" >
-          </div>
-          <div>id: {{ item.id}}
-          </div>
-          <div>Titolo: {{ item.title ? item.title : item.name }} 
-           </div>
-          <div>Titolo Originale: {{ item.original_title ? item.original_title : item.original_name }}  
-          </div>
-          <div>Lingua: {{ item.original_language}}</div>
-          <div v-if="item.original_language === 'en'">
-            Flag: <img :src="require('../assets/flag-en.png')" :alt="item.name">
-          </div>
-          <div v-else-if="item.original_language === 'it'">
-            Flag: <img :src="require('../assets/flag-it.png')" :alt="item.name">
-          </div>
-          <div v-else>
-          Flag: <img :src="require('../assets/flag-eu.png')" :alt="item.name">
-          </div> <br>
-          <div>
-            Voto:
-            <span v-for="(n, index) in 5" :key="index">
-              <i :class="n <= transformScale(item) ? 'fa-solid fa-star text-warning' : 'fa-regular fa-star' "></i>
-            </span>
-          </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            </div><!-- /poster -->
+          <!-- <div>id: {{ item.id}}
+          </div> -->
+          <div class="text-card">
+            <div>Titolo: {{ item.title ? item.title : item.name }}  </div>
+            <div>Titolo Originale: {{ item.original_title ? item.original_title : item.original_name }} </div>
+            <div>Lingua: {{ item.original_language}}
+              <span v-if="item.original_language === 'en'">
+                <img :src="require('../assets/flag-en.png')" :alt="item.name" class="flag">
+              </span>
+              <span v-else-if="item.original_language === 'it'">
+                <img :src="require('../assets/flag-it.png')" :alt="item.name">
+              </span>
+              <span v-else>
+              <img :src="require('../assets/flag-eu.png')" :alt="item.name">
+              </span>
+            </div> <!-- /lingua -->
+            <div>
+              Voto:
+              <span v-for="(n, index) in 5" :key="index">
+                <i :class="n <= transformScale(item) ? 'fa-solid fa-star text-warning' : 'fa-regular fa-star' "></i>
+              </span>
+            </div> <!-- /voto -->
+            <div>Overview: {{ item.overview }}  </div>
+          </div> <!-- /text-card -->
+        </div><!-- /v-for -->
+      </div><!-- /card-group -->
+    </div><!-- /container -->
 
 
 
@@ -113,7 +113,23 @@ export default {
   .card{
   background: #333;
   color: white;
-  width: 100%;
+  min-width: 250px;
+  min-height: 100%;
+  margin: 3px auto;
+  padding: 10px;
+  display: flex;
+  flex-flow: row wrap;
+  border: 1px solid yellow;
+  }
+  .text-card{
+    border: 1px solid violet;
+    width: 100%;
+    padding: 10px;
+    font-weight: 300;
+  }
+
+  .flag{
+    width: 50px;
   }
 
 }
